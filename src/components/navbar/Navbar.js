ed6df00f0b405541853adc840390a17cf9ac89e9
Rizu0007 <ruzwanali008@gmail.com>
2023-01-11 00:40:01 +0500

    ..

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -73,7 +73,7 @@ export default function Navbar() {
                         textTransform="uppercase"
                         color='teal'
                     >
-                        RIzu
+                        RIzu..
                     </CTypography>
                 </Stack>
 
